7c4948ad6765c85bec133864326737c97dd98d0a
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2026-04-02 19:04:38 +0200

    Add ring buffer header file with basic functions

---
+++ include/ring_buffer.h
@@ -0,0 +1,32 @@
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+#define RING_BUFFER_CAPACITY 8
+
+typedef enum
+{
+    RB_OK = 0,
+    RB_ERROR_NULL = 1,
+    RB_ERROR_FULL = 2,
+    RB_ERROR_EMPTY = 3
+} rb_status_t;
+
+typedef struct
+{
+    int data[RING_BUFFER_CAPACITY];
+    size_t head;
+    size_t tail;
+    size_t count;
+} ring_buffer_t;
+
+rb_status_t rb_init(ring_buffer_t *rb);
+rb_status_t rb_push(ring_buffer_t *rb, int value);
+rb_status_t rb_pop(ring_buffer_t *rb, int *value);
+
+bool rb_is_empty(const ring_buffer_t *rb);
+bool rb_is_full(const ring_buffer_t *rb);
+
+#endif
