3d16a395dac8fadc19cfa6b2d49854b72dce58d2
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2026-04-03 12:58:34 +0200

    Update rule08_macros_good.c

--- examples/rule08_macros_good.c
+++ examples/rule08_macros_good.c
@@ -3,12 +3,8 @@ static int square_good(int x)
     return x * x;
 }
 
-static int double_and_add_good(int a, int b)
-{
-    return (a * 2) + (b * 2);
-}
-
 int main(void)
 {
-    return square_good(2) + double_and_add_good(1, 2);
+    int result = square_good(1 + 2);
+    return (result == 9) ? 0 : 1;
 }
